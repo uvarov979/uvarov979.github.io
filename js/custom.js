0dac0be311e03f0662acd5ea9d36cd974be2e3b3
uvarov979 <andfly979@gmail.com>
2017-03-14 21:14:02 +0200

    14-03-2017(2step)

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,16 @@
+ 		(function($){
+
+ 	 /* ---------------------------------------------- /*
+	 * Preloader
+	 /* ---------------------------------------------- */
+
+	 $(window).load(function() {
+	 	$('#status').fadeOut();
+	 	$('#preloader').delay(300).fadeOut('slow');
+	 });
+
+	 $(document).ready(function() {
+
  		/* ---------------------------------------------- /*
 		 * Home BG
 		 /* ---------------------------------------------- */
@@ -27,6 +40,25 @@
 		 	offset: 70
 		 });
 
+		 /* ---------------------------------------------- /*
+		 * Skills
+        /* ---------------------------------------------- */    
+        //var color = $('#home').css('backgroundColor');
+
+        $('.skills').waypoint(function(){
+            $('.chart').each(function(){
+            $(this).easyPieChart({
+                    size:100,
+                    animate: 2000,
+                    lineCap:'butt',
+                    scaleColor: false,
+                    barColor: '#c6534f',
+                    trackColor: 'transparent',
+                    lineWidth: 10
+                });
+            });
+        },{offset:'80%'});
+
 		/* ---------------------------------------------- /*
 		 * Navbar Right
 		 /* ---------------------------------------------- */
@@ -42,22 +74,26 @@
 
 		 /* ---------------------------------------------- /*
 		 * Smooth scroll / Scroll To Top
-		/* ---------------------------------------------- */
+		 /* ---------------------------------------------- */
 
-		$('a[href*=#]').bind("click", function(e){
-           
-			var anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $(anchor.attr('href')).offset().top
-			}, 1000);
-			e.preventDefault();
+		 $('a[href*=#]').bind("click", function(e){
+
+		 	var anchor = $(this);
+		 	$('html, body').stop().animate({
+		 		scrollTop: $(anchor.attr('href')).offset().top
+		 	}, 1000);
+		 	e.preventDefault();
+		 });
+
+		 $(window).scroll(function() {
+		 	if ($(this).scrollTop() > 100) {
+		 		$('.scroll-up').fadeIn();
+		 	} else {
+		 		$('.scroll-up').fadeOut();
+		 	}
+		 });
+
 		});
 
-		$(window).scroll(function() {
-			if ($(this).scrollTop() > 100) {
-				$('.scroll-up').fadeIn();
-			} else {
-				$('.scroll-up').fadeOut();
-			}
-		});
+	})(jQuery);
 
